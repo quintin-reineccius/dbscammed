cb4ba1067325b8b7f82daef7fae391be6f850264
Quintin <quintinreineccius@gmail.com>
2015-05-16 21:43:16 -0700

    reverted

--- database.js
+++ database.js
@@ -1,17 +1,12 @@
 var mysql      = require('mysql');
-if(process.env.CLEARDB_DATABASE_URL){
-	var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
-}
-else{
-	var connection = mysql.createConnection({
-	  host     : process.env.DB_HOST,
-	  user     : process.env.DB_USER,
-	  password : process.env.DB_PASS,
-	  database : process.env.DB,
-	  port     : process.env.DB_PORT
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB,
+  port     : process.env.DB_PORT
 
-	});
-}
+});
 
 connection.connect();
 module.exports = connection
